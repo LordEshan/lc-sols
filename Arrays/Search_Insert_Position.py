d39eddb59de776bcc7cd12f60615ffe036fca63e
Eshan Chowdhury <eshan180405@gmail.com>
2026-08-29 11:20:29 +0530

    Update Search Insert Position comments

--- Arrays/Search_Insert_Position.py
+++ Arrays/Search_Insert_Position.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def searchInsert(self, nums, target):
         left = 0
-        right = len(nums)-1
+        right = len(nums)-1                  #get the mid index for floor division giving only integer values
 
         while left <= right:
-            mid = (left+right)//2
+            mid = (left+right)//2       #infinite loop until the left index is less than or equal to the right index
 
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            elif nums[mid] < target:           #check right
                 left = mid + 1
         
-            elif nums[mid] > target:
+            elif nums[mid] > target:           #check left or you could just use else statement here
                 right = mid - 1
                 
-        return left
+        return left             
     
         
